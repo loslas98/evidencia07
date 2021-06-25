17040d39fdeae6d6a4539fd84c57343a0a84cf2a
loslas98 <m.divin.u@gmail.com>
2021-06-25 02:20:15 -0400

    se instancia un objeto trabajador en main appLauncher

--- src/main/java/ventana/appLauncher.java
+++ src/main/java/ventana/appLauncher.java
@@ -1,8 +1,15 @@
 package ventana;
 
+import modelo.Trabajador;
+
 public class appLauncher {
     public static void main(String[] args) {
         GuiTrabajador gui = new GuiTrabajador();
         gui.setVisible(true);
+        Trabajador trabajador = new Trabajador("james hetfield","15.456.345-7","mas vida","cuprum");
+        System.out.println(trabajador.getNombre());
+        System.out.println(trabajador.getRut());
+        System.out.println(trabajador.getAfp());
+        System.out.println(trabajador.getIsapre());
     }
 }
